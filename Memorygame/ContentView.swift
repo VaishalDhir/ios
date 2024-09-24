2d0d361c654bfc3ca12d5f98d629ca928339d68e
Vaishal <vaishaldhir4@gmail.com>
2024-09-24 06:54:08 -0600

    In this commit we learn about  State, array and toggle

--- Memorygame/ContentView.swift
+++ Memorygame/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻", "🎃", "👽", "😈", "🫣"] // Defining array to store emojis for the game.
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            //indices means Range
+            ForEach (emojis.indices, id:\.self) { index in
+                CardView(content: emojis[index])
+                
+            }
+            // Using ForEach instead of writing again and again.
+//            CardView(content: "👻")
+//            CardView(content: "🎃")
+//            CardView(content: "👽")
+//            CardView(content: "😈")
         }.foregroundColor(.orange)
        
         .padding()
@@ -21,20 +28,25 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    // State creates a pointer towards isFaceUp. Has to do with the displaying not with the actual app.
+    let content: String
+    @State var isFaceUp: Bool = false          // Can also remove Bool as swift is a strongly typed language.
+    let base = RoundedRectangle(cornerRadius: 12) // Gave roundedRectangle to base as it will be easy and looks good.
     var body: some View {
         ZStack (content:  {
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 4)
-                Text("👻").font(.largeTitle)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 4)
+                Text(content).font(.largeTitle)
             }
             else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
-            
         }
-   ) }
+        ).onTapGesture {                // to add tap on the screen
+            isFaceUp.toggle()           // toggle means to flip
+        }
+    }
 }
 
 
